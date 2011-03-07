0483e216f2d14880a77ff520be44aaa5e30ac9de
Caley Woods <caley.w@gmail.com>
2011-03-06 22:43:10 -0600

    moar cleanup

--- test/functional/links_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class LinksControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
-  end
-
-end
--- test/unit/helpers/voteables_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class VoteablesHelperTest < ActionView::TestCase
-end
