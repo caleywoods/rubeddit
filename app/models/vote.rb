1eacfa52b39059631bcea9e40967a96768253f8b
Caley Woods <caley.w@gmail.com>
2011-03-06 22:37:21 -0600

    added unique vote checking, kinda ugly for now

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,7 @@ class Vote < ActiveRecord::Base
   belongs_to :link
   belongs_to :user
 
+validates_uniqueness_of :link_id, :scope => :user_id, 
+    :message => "can only be voted once per user"
+
 end
